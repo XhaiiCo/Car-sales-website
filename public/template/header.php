6881b7a329adffd965bdf93aafa1a522d345159e
Leclercq David <davidleclercq2002@gmail.com>
2022-03-19 13:00:00 +0100

    First test: layout of the ad page

--- public/template/header.php
+++ public/template/header.php
@@ -18,10 +18,10 @@
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="ad.php">Annonces</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" href="#">Disabled</a>
