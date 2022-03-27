d27868f136e5f071ba17a0b5cfa0c5472e52044f
David Leclercq <davidleclercq2002@gmail.com>
2022-03-27 21:31:59 +0200

    Added a method signature for the authController

---
+++ src/util/authController.php
@@ -0,0 +1,29 @@
+<?php
+
+function login($mail, $password)
+{
+}
+
+function logout()
+{
+}
+
+function startSession()
+{
+}
+
+function isConnected()
+{
+}
+
+function isGestion()
+{
+}
+
+function isSeller()
+{
+}
+
+function getUsername()
+{
+}
