71738ef75e09d4b78736655dc47efedacdf7a77e
Leclercq David <davidleclercq2002@gmail.com>
2022-03-30 16:51:29 +0200

    Continue the admin users page

--- src/view/admin/v_admin_user.php
+++ src/view/admin/v_admin_user.php
@@ -46,7 +46,7 @@
                                 <tr>
                                     <th><span>Utilisateur</span></th>
                                     <th><span>Creation</span></th>
-                                    <th class="text-center"><span>Rôle</span></th>
+                                    <th><span>Rôle</span></th>
                                     <th><span>Email</span></th>
                                     <th>&nbsp;</th>
                                 </tr>
@@ -73,6 +73,10 @@
                 displayUser(datas);
             }
         });
+
+        $(".remove").click(function() {
+            console.log("here");
+        });
     });
 
 
@@ -85,31 +89,55 @@
             if (data.isSeller === "1") {
                 role += ", Vendeur"
             }
+
+            tr = document.createElement("tr");
+
+            tdUsername = document.createElement("td");
+            $(tdUsername).html(data.username);
+            tr.append(tdUsername);
+
+            tdDate = document.createElement("td");
+            $(tdDate).html(data.date_registration);
+            tr.append(tdDate);
+
+            tdRole = document.createElement("td");
+            $(tdRole).html(role);
+            tr.append(tdRole);
+
+            tdEmail = document.createElement("td");
+            $(tdEmail).html(data.user_mail);
+            tr.append(tdEmail);
+
+            tdBtnContainer = document.createElement("td");
+            $(tdBtnContainer).attr("style", "width:20%");
+
+            spanModif = document.createElement("span");
+            $(spanModif).attr("style", "table-link text-info fa-stack");
+            $(spanModif).html("<i class='fa fa-square fa-stack-2x'></i><i class='fa fa-pencil fa-stack-1x fa-inverse'></i> ");
+
+            tdBtnContainer.append(spanModif);
+            tr.append(tdBtnContainer);
+
             const html =
                 "                    <tr> \
                                     <td> " + data.username + "</td> \
                                     <td> " + data.date_registration + "</td> \
-                                    <td class='text-center'> \
-                                        <span class='label label-default'>" + role + "</span> \
-                                    </td> \
+                                    <td> " + role + "</td> \
                                     <td>" + data.user_mail + "</td> \
                                     <td style='width: 20%;'> \
-                                        <a href='#' class='table-link text-info'> \
-                                            <span class='fa-stack'> \
-                                                <i class='fa fa-square fa-stack-2x'></i> \
-                                                <i class='fa fa-pencil fa-stack-1x fa-inverse'></i> \
-                                            </span> \
-                                        </a> \
-                                        <a href='#' class='table-link danger'> \
-                                            <span class='fa-stack'> \
-                                                <i class='fa fa-square fa-stack-2x'></i> \
-                                                <i class='fa fa-trash-o fa-stack-1x fa-inverse'></i> \
-                                            </span> \
-                                        </a> \
+                                        <span class='table-link text-info fa-stack'> \
+                                            <i class='fa fa-square fa-stack-2x'></i> \
+                                            <i class='fa fa-pencil fa-stack-1x fa-inverse'></i> \
+                                        </span> \
+                                        <span class='table-link text-danger fa-stack'> \
+                                            <i class='fa fa-square fa-stack-2x'></i> \
+                                            <i class='fa fa-trash-o fa-stack-1x fa-inverse'></i> \
+                                        </span> \
                                     </td> \
                                 </tr> \
                                 ";
-            $("#user-container").append(html);
+            $("#user-container").append(tr);
+
         }
 
     }
