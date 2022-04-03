fd64bdfec81fd15960aa3948bac2e04f4bacf491
David Leclercq <davidleclercq2002@gmail.com>
2022-04-03 11:11:32 +0200

    some security

--- public/model/model_remove_user.php
+++ public/model/model_remove_user.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once "../../src/controller/authController.php";
+
+if (!isAdmin()) {
+    exit();
+}
+
 $user = $_POST['user'];
 
 require_once "../../src/util/db.php";
--- public/model/model_user.php
+++ public/model/model_user.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once "../../src/controller/authController.php";
+
+if (!isAdmin()) {
+    exit();
+}
+
 require_once "../../src/util/db.php";
 
 if (!isset($_POST))
