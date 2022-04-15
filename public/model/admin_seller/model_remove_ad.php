f01c5956ab9d42ccf9c8e7779b43dbeb73ce16eb
David Leclercq <davidleclercq2002@gmail.com>
2022-04-15 11:04:21 +0200

    Before deleting an ad from a seller check that the ad is really his

--- public/model/admin_seller/model_remove_ad.php
+++ public/model/admin_seller/model_remove_ad.php
@@ -5,12 +5,27 @@ require_once "../../../src/controller/authController.php";
 if (!isSeller() && !isAdmin()) {
     exit();
 }
+$id = $_POST["id"];
 
 require_once "../../../src/util/db.php";
+
+//Before deleting an ad from a seller check that the ad is really his
+if (isSeller()) {
+    $sql = "select id_sale from sale where id_sale = :id and user_mail = :mail";
+    $params = [
+        "id" => $id,
+        "mail" => getEmail()
+    ];
+
+    $exist = prepare($sql, $params);
+    if (empty($exist)) {
+        exit();
+    }
+}
 
 if (!isset($_POST))
     exit();
 
 $sql = "delete from sale where id_sale = :id";
 
-prepare($sql, ["id" => $_POST["id"]]);
+prepare($sql, ["id" => $id]);
