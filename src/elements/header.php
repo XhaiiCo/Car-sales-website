6b1ae42a0316b66b6c5c8a575d2cc3b2cc6fdf8a
David Leclercq <davidleclercq2002@gmail.com>
2022-04-15 19:06:01 +0200

    Add a favicon

--- src/elements/header.php
+++ src/elements/header.php
@@ -9,6 +9,7 @@
             cursor: pointer;
         }
     </style>
+    <link rel="icon" type="image/x-icon" href="./assets/img/site_elements/favicon/favicon.png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
