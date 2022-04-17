4a1ef8cc1a84e745e6c7a2688aa5c0e21401f307
David Leclercq <davidleclercq2002@gmail.com>
2022-04-17 15:10:13 +0200

    A user can now send a message to a seller

--- public/model/logged_user/model_send_message.php
+++ public/model/logged_user/model_send_message.php
@@ -20,19 +20,17 @@ if (empty($user_to)) {
     leave(["error" => 1, "em" => "Erreur: pas de vendeur trouvé"]);
 }
 
-// $sql = "
-//     INSERT IGNORE INTO message_sale VALUES (NULL, :message, :user_from, :user_to, :id_sale)
-// ";
+$sql = "
+    INSERT IGNORE INTO message_sale VALUES (NULL, :message, :user_from, :user_to, :id_sale)
+";
+$params = [
+    "message" => $message,
+    "user_from" => $user_from,
+    "user_to" => $user_to,
+    "id_sale" => $id
+];
 
-// $params = [
-//     "message" => $message,
-//     "user_from" => $user_from,
-//     "user_to" => $user_to,
-//     "id_sale" => $id
-// ];
-
-// prepare($sql, $params);
-
+prepare($sql, $params);
 leave(["success" => 1, "sm" => "Message envoyé avec succès"]);
 
 function leave($response)
--- src/util/db.php
+++ src/util/db.php
@@ -14,7 +14,6 @@ function getDB()
 
         try {
             $bdd = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $user, $pass);
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (Exception $e) {
         }
     }
